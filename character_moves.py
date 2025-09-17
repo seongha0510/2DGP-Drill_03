653b7c095e6156ebfeccd1aa5625af37536ad487
seongha0510 <kingsh0510@tukorea.ac.kr>
2025-09-17 17:56:01 +0900

    사각운동 기본뼈대 완성

--- character_moves.py
+++ character_moves.py
@@ -4,9 +4,25 @@ open_canvas()
 
 boy=load_image('character.png')
 
+def move_top():
+    pass
+
+def move_right():
+    pass
+
+def move_bottom():
+    pass
+
+def move_left():
+    pass
+
 
 def move_rectangle():
     print("MOVING RECTANGLE")
+    move_top
+    move_right
+    move_bottom
+    move_left
     pass
 
 
