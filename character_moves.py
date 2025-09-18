9d0e3d6d106b3f9132a559a51d26f09b5308b22d
seongha0510 <kingsh0510@tukorea.ac.kr>
2025-09-18 17:23:44 +0900

    삼각운동 추가

--- character_moves.py
+++ character_moves.py
@@ -47,6 +47,25 @@ def move_circle():
         draw_boy(x, y)
     pass
 
+def move_triangle():
+    print("MOVING TRIANGLE")
+    x1, y1 = 400, 550
+    x2, y2 = 800, 50
+    x3, y3 = 0, 50
+    for t in range(0, 101):
+        x = x1 + (x2 - x1) * t / 100
+        y = y1 + (y2 - y1) * t / 100
+        draw_boy(x, y)
+    for t in range(0, 101):
+        x = x2 + (x3 - x2) * t / 100
+        y = y2 + (y3 - y2) * t / 100
+        draw_boy(x, y)
+    for t in range(0, 101):
+        x = x3 + (x1 - x3) * t / 100
+        y = y3 + (y1 - y3) * t / 100
+        draw_boy(x, y)
+    pass
+
 def draw_boy(x: float, y: float):
     clear_canvas_now()
     boy.draw_now(x, y)
@@ -55,6 +74,7 @@ def draw_boy(x: float, y: float):
 
 while True:
     move_rectangle()
+    move_triangle()
     move_circle()
     break
     pass
